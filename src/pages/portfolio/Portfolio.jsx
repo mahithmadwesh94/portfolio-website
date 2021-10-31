2072ff71fc20a61d23129c71d6ec5f0607d89b10
mahith.madwesh <45761582+mahithmadwesh94@users.noreply.github.com>
2021-10-31 12:28:22 +0530

    added porfolio sites

--- src/pages/portfolio/Portfolio.jsx
+++ src/pages/portfolio/Portfolio.jsx
@@ -3,6 +3,8 @@ import { ReactComponent as Netlify } from "../../assets/icons/netlify.svg"
 import { ReactComponent as Git } from "../../assets/icons/github.svg"
 import { Col, Container, Row } from "react-bootstrap";
 import Footer from "../../components/footer/Footer";
+import mtravel from "../../assets/images/mTravelAppPin.png"
+import ecommerSite from "../../assets/images/ecommerceSite.png"
 
 
 function Portfolio() {
@@ -18,15 +20,15 @@ function Portfolio() {
                     <Col sm={12} md={6} lg={4}>
                         <div className="portfolioContainer">
 
-                            <img src="/assets/images/portfolio1.png" alt="" className="portfolioImg" />
+                            <img src={mtravel} alt="" className="portfolioImg" />
 
                             <div className="portolfolioPopup ">
                                 <div className="siteLinksWrapper ">
                                     <div className="siteLinkWrapper">
-                                        <Netlify className="siteLink" /> <span>Site</span>
+                                        <a href="https://adoring-lamport-2a07ea.netlify.app/" target="_blank" rel="noreferrer"><Netlify className="siteLink" /> <span>Site</span></a>
                                     </div>
                                     <div className="siteCodeWrapper">
-                                        <Git className="siteCode" /> <span>Code</span>
+                                        <a href="https://github.com/mahithmadwesh94/mtravel-frontend" target="_blank" rel="noreferrer"><Git className="siteCode" /> <span>Code</span></a>
                                     </div>
                                 </div>
                             </div>
@@ -42,10 +44,10 @@ function Portfolio() {
                             <div className="portolfolioPopup ">
                                 <div className="siteLinksWrapper ">
                                     <div className="siteLinkWrapper">
-                                        <Netlify className="siteLink" /> <span>Site</span>
+                                        <a href="https://business-mgmnt-4e847f.netlify.app/" target="_blank" rel="noreferrer"> <Netlify className="siteLink" /> <span>Site</span></a>
                                     </div>
                                     <div className="siteCodeWrapper">
-                                        <Git className="siteCode" /> <span>Code</span>
+                                        <a href="https://github.com/mahithmadwesh94/business-mgmnt-ui" target="_blank" rel="noreferrer"><Git className="siteCode" /> <span>Code</span></a>
                                     </div>
                                 </div>
                             </div>
@@ -56,53 +58,15 @@ function Portfolio() {
                     <Col sm={12} md={6} lg={4}>
                         <div className="portfolioContainer">
 
-                            <img src="/assets/images/portfolio1.png" alt="" className="portfolioImg" />
+                            <img src={ecommerSite} alt="" className="portfolioImg" />
 
                             <div className="portolfolioPopup ">
                                 <div className="siteLinksWrapper ">
                                     <div className="siteLinkWrapper">
-                                        <Netlify className="siteLink" /> <span>Site</span>
+                                        <a href="https://pensive-khorana-c2d34d.netlify.app/" target="_blank" rel="noreferrer"><Netlify className="siteLink" /> <span>Site</span></a>
                                     </div>
                                     <div className="siteCodeWrapper">
-                                        <Git className="siteCode" /> <span>Code</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </Col>
-
-
-                    <Col sm={12} md={6} lg={4}>
-                        <div className="portfolioContainer">
-
-                            <img src="/assets/images/portfolio1.png" alt="" className="portfolioImg" />
-
-                            <div className="portolfolioPopup ">
-                                <div className="siteLinksWrapper ">
-                                    <div className="siteLinkWrapper">
-                                        <Netlify className="siteLink" /> <span>Site</span>
-                                    </div>
-                                    <div className="siteCodeWrapper">
-                                        <Git className="siteCode" /> <span>Code</span>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </Col>
-
-
-                    <Col sm={12} md={6} lg={4}>
-                        <div className="portfolioContainer">
-
-                            <img src="/assets/images/portfolio1.png" alt="" className="portfolioImg" />
-
-                            <div className="portolfolioPopup ">
-                                <div className="siteLinksWrapper ">
-                                    <div className="siteLinkWrapper">
-                                        <Netlify className="siteLink" /> <span>Site</span>
-                                    </div>
-                                    <div className="siteCodeWrapper">
-                                        <Git className="siteCode" /> <span>Code</span>
+                                        <a href="https://github.com/mahithmadwesh94/ecommerce-frontend" target="_blank" rel="noreferrer"><Git className="siteCode" /> <span>Code</span></a>
                                     </div>
                                 </div>
                             </div>
