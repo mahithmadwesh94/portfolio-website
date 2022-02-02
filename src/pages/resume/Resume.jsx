d8c2b561c7053a2a67a73ddf9c9ae09a6fb98c2a
mahith.madwesh <45761582+mahithmadwesh94@users.noreply.github.com>
2022-02-02 21:10:08 +0530

    Update Resume link

--- src/pages/resume/Resume.jsx
+++ src/pages/resume/Resume.jsx
@@ -12,7 +12,7 @@ function Resume() {
                 <div className="resumeHeader">
 
                     <div>Resume</div>
-                    <a href="https://drive.google.com/file/d/1Cb0cAIyyLl0mSk-AUJJf59MDsC88VOPg/view?usp=sharing" target="_blank" rel="noreferrer" style={{ color: 'white' }} download><Button className="aboutContact">Download</Button></a>
+                    <a href="https://drive.google.com/file/d/1JlsuXr9Bn1RKVJKL-HxUHuD4z1LFj32A/view?usp=sharing" target="_blank" rel="noreferrer" style={{ color: 'white' }} download><Button className="aboutContact">Download</Button></a>
                     <span>3 Years Experience</span>
                 </div>
             </Container>
